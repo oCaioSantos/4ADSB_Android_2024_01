ef5f5aef0e383cf9d62f2d667607cd3ac2bfad40
oCaioSantos <caio.santos@sptech.school>
2024-02-23 12:14:50 -0300

    Conteúdo da aula

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "1502_4ADSB"
+include(":app")
